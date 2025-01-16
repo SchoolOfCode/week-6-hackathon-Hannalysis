11989b9c835e5309047bbb15bed42ce7806c1a80
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2025-01-16 16:41:53 +0000

    First Kata Completed with Tests
    Just requires instructions and documentation.

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-/* --- Counter Strike: GO --- */
+/* --- Counter Strike: Source/GO --- */
 
-//Easy Kata 2
+ /* --- Kata 1 [Easy] --- */
 //When a player gets a damage buff, the function will calculate a multiplier effect, and returns the increased damage the player will do (possible parameter: player damage. Or no parameter)
 
-//Challenging Kata 1
+export function damageBuff (playerDamage) {
+// enter code here ready to test against
+
+// return playerDamage with increased buff amount
+}
+
+ /* --- Kata 2 [Challenging] --- */
 // Terrorist and Counter Terrorist separate arrays of available weaponry
 //The challenge would be to input a weapon name and the function would return what team can equip it
 
+export function teamWeapon (weapon) {
+    // enter code here ready to test against
+    // return the team that is able to utilize that weapon
+    }
--- main.test.js
+++ main.test.js
@@ -1,4 +1,70 @@
 //  import `test` and `expect` functions from Vitest
 import { expect, test } from "vitest"
 //  import the PH kata functions from main.js
-import { } from "./main.js"
+import { damageBuff } from "./main.js"
+
+ /* TC#1 */
+test('Whole positive number (50)', () => {
+    // Arrange
+    const input = 50;
+    const expected = 75;
+    // Act
+    const result = damageBuff(input);
+    //Assert 
+    expect(result).toBe(expected);   
+    });
+
+ /* TC#2 */
+test('Whole positive number, max amount (100)', () => {
+    // Arrange
+    const input = 100;
+    const expected = 150;
+    //Act
+    const result = damageBuff(input);
+    //Assert
+    expect(result).toBe(expected);   
+    });
+
+ /* TC#3 */    
+test('Whole positive number, min amount (1)', () => {
+    // Arrange
+    const input = 1;
+    const expected = 1.5;
+    //Act
+    const result = damageBuff(input);
+    //Assert
+    expect(result).toBe(expected);   
+    });
+
+ /* TC#4 */    
+ test('When the input is 0, treat it as the min input amount (1)', () => {
+    // Arrange
+    const input = 0;
+    const expected = 1.5;
+    //Act
+    const result = damageBuff(input);
+    //Assert
+    expect(result).toBe(expected);   
+    });
+
+ /* TC#5 */    
+ test('When the input is negative, treat it as the min input amount (1)', () => {
+    // Arrange
+    const input = -33;
+    const expected = 1.5;
+    //Act
+    const result = damageBuff(input);
+    //Assert
+    expect(result).toBe(expected);   
+    });
+
+ /* TC#6 */    
+ test('When the input is a number as a string, it is converted to a number', () => {
+    // Arrange
+    const input = "50";
+    const expected = 75;
+    //Act
+    const result = damageBuff(input);
+    //Assert
+    expect(result).toBe(expected);   
+    });
